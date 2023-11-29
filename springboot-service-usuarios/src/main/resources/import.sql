2dd6ad33cd4f5dbb90a4e19bc4f79e576a5d1c9c
JustDigits <112163363+JustDigits@users.noreply.github.com>
2023-11-29 01:18:49 -0600

    RepositoryRestResource HATEOAS

---
+++ springboot-service-usuarios/src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('justdigits', '12345', 1, 'Axell', 'Zuniga', 'test@gmail.com');
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('nissan', '12345', 1, 'Nissan', 'Gtr', 'nissan@gmail.com');
